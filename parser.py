1ccb7ac4af04293e7dff14bdcd5d59d4e42fc096
lrinaudo <leandro.rinaudo@unq.edu.ar>
2019-09-16 14:46:19 -0300

    agrego signatura, precondicion y postcondicion

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 from sly import Lexer, Parser
+import json
 
 class CalcLexer(Lexer):
-    tokens = { FUN, LOWERID, UPPERID, ARROW, COMMA }
+    tokens = { FUN, LOWERID, UPPERID, ARROW, COMMA, SIGNATURE, PRECONDITION, POSTCONDITION }
     ignore = ' \t'
 
     # Tokens
@@ -10,6 +11,9 @@ class CalcLexer(Lexer):
     UPPERID = r'[A-Z][_a-zA-Z0-9]*'
     ARROW = r'->'
     COMMA = r','
+    SIGNATURE = r':'
+    PRECONDITION = r'\?'
+    POSTCONDITION = r'!'
 
     # Ignored pattern
     ignore_newline = r'\n+'
@@ -41,13 +45,25 @@ class CalcParser(Parser):
     def declaraciones(self, p):
         return []
     
-    @_('FUN LOWERID regla')
+    @_('FUN LOWERID signatura')
     def declaracion(self, p):
-        return ('fun', p.LOWERID, p.regla)
+        return ('fun', p.LOWERID, p.signatura)
 
     @_('UPPERID ARROW UPPERID')
     def regla(self, p):
         return ('rule', p.UPPERID0, p.UPPERID1)
+
+    @_('SIGNATURE LOWERID precondicion')
+    def signatura(self, p):
+        return ('sig', p.LOWERID, p.precondicion)
+
+    @_('PRECONDITION LOWERID postcondicion')
+    def precondicion(self, p):
+        return ('pre', p.LOWERID, p.postcondicion)
+
+    @_('POSTCONDITION LOWERID regla')
+    def postcondicion(self, p):
+        return ('post', p.LOWERID, p.regla)
 
     @_('')
     def empty(self, p):
@@ -57,12 +73,19 @@ if __name__ == '__main__':
     data = '''
 -- esto es un comentario
 fun length
+: sadsdasdasdasd
+? blabla
+! asdasds
     Nil -> Zero
 fun length2
+: sig2
+? balasdasd
+! asdasdasdasdasdas    
     Nil2 -> Zero2
 '''
     lexer = CalcLexer()
     parser = CalcParser()
     # for tok in lexer.tokenize(data):
     #     print('type=%r, value=%r' % (tok.type, tok.value))
-    print(parser.parse(lexer.tokenize(data)))
+    print(json.dumps(parser.parse(lexer.tokenize(data))))
+    # print(parser.parse(lexer.tokenize(data)))
